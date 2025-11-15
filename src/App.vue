7911c24386909edb87beeaa365408e69f4aa1f61
Dernoa <125189982+Dernoa@users.noreply.github.com>
2025-11-15 20:41:10 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div><button @click="addLike">Like</button></div>
+        <div>Кол-во лайков {{likes}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                likes: 0,
+            }
+        },
+        methods: {
+            addLike(){
+                this.likes += 1;
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
